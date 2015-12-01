afbce198f6109628d13d37caf512e86cd53344a3
ledu0003@e.ntu.edu.sg <ledu0003@e.ntu.edu.sg>
2015-12-01 21:40:30 +0800

    dcm

--- EasySports/easySports/build/generated/source/r/debug/com/facebook/R.java
+++ EasySports/easySports/build/generated/source/r/debug/com/facebook/R.java
@@ -103,7 +103,7 @@ public final class R {
 		public static final int inline = 0x7f0d008b;
 		public static final int large = 0x7f0d008f;
 		public static final int left = 0x7f0d000a;
-		public static final int messenger_send_button = 0x7f0d017e;
+		public static final int messenger_send_button = 0x7f0d0181;
 		public static final int never_display = 0x7f0d008e;
 		public static final int normal = 0x7f0d0014;
 		public static final int open_graph = 0x7f0d0085;
